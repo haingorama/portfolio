d23abda4add8862862bd33c26cb8f5a4a8000bde
haingorama <haingo.ramaroson.pro@gmail.com>
2022-12-02 22:50:54 +0100

    Ajout Formations

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 var scene = document.getElementById('scene');
 				var parallax = new Parallax(scene);
+
+// FORMATION
+lightbox.option({
+resizeDuration: 300,
+wrapAround: true,
+disableScrolling: true,
+albumLabel: "%1 / %2",
+});
