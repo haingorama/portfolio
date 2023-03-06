eb0937c112442eb35c554cc1ba485c9dc61d9853
haingorama <haingo.ramaroson.pro@gmail.com>
2023-03-06 19:10:41 +0100

    Ecoco modif

--- js/ecoco.js
+++ js/ecoco.js
@@ -1,88 +1,22 @@
+const $backTop = $(".back-to-top");
+const isHidden = "is-hidden";
 
-const elts = {
-text1: document.getElementById("text1"),
-text2: document.getElementById("text2") };
+AOS.init({
+  offset: 200,
+  delay: 50,
+  once: true
+});
 
-const texts = [
-"Création ",
-"d'un",
-"site",
-"web",
-"e-commerce",
-"\"Ecoco\""];
+$(window).on("scroll", function() {
+  const $this = $(this);
+  if ($this.scrollTop() + $this.height() == $(document).height()) {
+    $backTop.removeClass(isHidden);
+  } else {
+    $backTop.addClass(isHidden);
+  }
+});
 
-// Controls the speed of morphing.
-const morphTime = 1;
-const cooldownTime = 0.25;
-
-let textIndex = texts.length - 1;
-let time = new Date();
-let morph = 0;
-let cooldown = cooldownTime;
-
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-
-function doMorph() {
-morph -= cooldown;
-cooldown = 0;
-
-let fraction = morph / morphTime;
-
-if (fraction > 1) {
-    cooldown = cooldownTime;
-    fraction = 1;
-}
-
-setMorph(fraction);
-}
-
-// A lot of the magic happens here, this is what applies the blur filter to the text.
-function setMorph(fraction) {
-// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
-
-elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-fraction = 1 - fraction;
-elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-}
-
-function doCooldown() {
-morph = 0;
-
-elts.text2.style.filter = "";
-elts.text2.style.opacity = "100%";
-
-elts.text1.style.filter = "";
-elts.text1.style.opacity = "0%";
-}
-
-// Animation loop, which is called every frame.
-function animate() {
-requestAnimationFrame(animate);
-
-let newTime = new Date();
-let shouldIncrementIndex = cooldown > 0;
-let dt = (newTime - time) / 1000;
-time = newTime;
-
-cooldown -= dt;
-
-if (cooldown <= 0) {
-    if (shouldIncrementIndex) {
-    textIndex++;
-    }
-
-    doMorph();
-} else {
-    doCooldown();
-}
-}
-
-// Start the animation.
-animate();
+$backTop.on("click", () => {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
